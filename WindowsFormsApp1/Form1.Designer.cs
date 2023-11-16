c5c774fc185d5e470aecef967e6466cb2ab1b87e
fitthuctap05 <fitthuctap05@fpts.com.vn>
2023-11-16 09:46:37 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_score = new System.Windows.Forms.Label();
+            this.lbl_over = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ship = new System.Windows.Forms.PictureBox();
+            this.alien = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alien)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_score
+            // 
+            this.lbl_score.AutoSize = true;
+            this.lbl_score.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_score.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_score.Location = new System.Drawing.Point(1, 9);
+            this.lbl_score.Name = "lbl_score";
+            this.lbl_score.Size = new System.Drawing.Size(55, 13);
+            this.lbl_score.TabIndex = 3;
+            this.lbl_score.Text = "Score: 0";
+            // 
+            // lbl_over
+            // 
+            this.lbl_over.AutoSize = true;
+            this.lbl_over.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_over.Font = new System.Drawing.Font("Ravie", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_over.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_over.Location = new System.Drawing.Point(174, 202);
+            this.lbl_over.Name = "lbl_over";
+            this.lbl_over.Size = new System.Drawing.Size(120, 17);
+            this.lbl_over.TabIndex = 4;
+            this.lbl_over.Text = "MÀY NGU LẮM";
+            this.lbl_over.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.vu_truj;
+            this.pictureBox1.Location = new System.Drawing.Point(-9, -31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ship
+            // 
+            this.ship.Image = global::WindowsFormsApp1.Properties.Resources.Pasted_20231113_095035_preview_rev_1;
+            this.ship.Location = new System.Drawing.Point(338, 49);
+            this.ship.Name = "ship";
+            this.ship.Size = new System.Drawing.Size(42, 57);
+            this.ship.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ship.TabIndex = 2;
+            this.ship.TabStop = false;
+            this.ship.Tag = "enemy";
+            // 
+            // alien
+            // 
+            this.alien.Image = global::WindowsFormsApp1.Properties.Resources.Pasted_20231113_094736_preview_rev_1;
+            this.alien.Location = new System.Drawing.Point(94, 49);
+            this.alien.Name = "alien";
+            this.alien.Size = new System.Drawing.Size(48, 55);
+            this.alien.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.alien.TabIndex = 1;
+            this.alien.TabStop = false;
+            this.alien.Tag = "enemy";
+            // 
+            // player
+            // 
+            this.player.Image = global::WindowsFormsApp1.Properties.Resources.meo_dong_lao_preview_rev_1;
+            this.player.Location = new System.Drawing.Point(202, 384);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(60, 65);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.lbl_over);
+            this.Controls.Add(this.lbl_score);
+            this.Controls.Add(this.ship);
+            this.Controls.Add(this.alien);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ship)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alien)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox alien;
+        private System.Windows.Forms.PictureBox ship;
+        private System.Windows.Forms.Label lbl_score;
+        private System.Windows.Forms.Label lbl_over;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl_over.Hide();
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    // Khởi tạo VlcControl
+        //    VlcControl vlcControl = new VlcControl();
+        //    vlcControl.BeginInit();
+        //    vlcControl.VlcLibDirectory = new DirectoryInfo(@"E:\Bài Tập FPTS\Plane\WindowsFormsApp1\packages");
+        //    vlcControl.EndInit();
+
+        //    // Thêm VlcControl vào Form
+        //    Controls.Add(vlcControl);
+
+        //    // Đặt đường dẫn của video
+        //    string videoPath = @"E:\Video\nebula_red_-_31420 (Original)"; 
+
+        //    vlcControl.SetMedia(new FileInfo(videoPath));
+        //    vlcControl.Play();
+        //}
+
+
+        bool right, left, space;
+        int score;
+
+        void Game_Retsult() 
+        {
+            foreach (Control x in this.Controls)
+            {
+                foreach (Control i in this.Controls)
+                {
+                    if (x is PictureBox && x.Tag == "bullet")
+                    {
+                        if (i is PictureBox && i.Tag=="enemy")
+                        {
+                            if (x.Bounds.IntersectsWith(i.Bounds))
+                            {
+                                i.Top = -100;
+                                ((PictureBox)x).Image = Properties.Resources.explosion;
+                                score++;
+                                lbl_score.Text ="Score:"+score;
+
+
+
+
+
+
+                            }
+                        }
+                    }
+                }
+            }
+            if (player.Bounds.IntersectsWith(ship.Bounds) ||player.Bounds.IntersectsWith(alien.Bounds))
+            {
+                timer1.Stop();
+                lbl_over.Show();
+                lbl_over.BringToFront();
+            }
+        }
+
+        void Add_Bullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.SizeMode = PictureBoxSizeMode.AutoSize;
+            bullet.Image = Properties.Resources.bullet_img2;
+            bullet.BackColor = System.Drawing.Color.Transparent;
+            bullet.Tag = "bullet";
+            bullet.Left = player.Left + 10;
+            bullet.Top = player.Top -15;
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
+        }
+
+        void Buleet_Movement()
+        {
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag == "bullet")
+                {
+                    x.Top -= 10;
+                    if (x.Top<100)
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        void Enemy_Movement()
+        {
+            Random rnd = new Random();
+            int x, y;
+            if (alien.Top>=500)
+            {
+                x = rnd.Next(0,300);
+                alien.Location = new Point(x,0);
+            }
+            if (ship.Top>=500)
+            {
+                y = rnd.Next(0, 300);
+                ship.Location = new Point(y,0);
+            }
+            else
+            {
+                alien.Top += 15;
+                ship.Top += 10;
+            }
+        }
+
+        void Arrow_key_Movement()
+        {
+            if (right == true)
+            {
+                if (player.Left<425) 
+                {
+                    player.Left += 20;
+                }
+            }
+            if (left == true)
+            {
+                if (player.Left>10)
+                {
+                    player.Left -= 20;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode== Keys.Right)
+            {
+                right = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                space = true;
+                Add_Bullet();
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                space = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Arrow_key_Movement();
+            Enemy_Movement();
+            Buleet_Movement();
+            Game_Retsult();
+        }
+
+    }
+}
